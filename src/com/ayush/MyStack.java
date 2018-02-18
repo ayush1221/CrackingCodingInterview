17d9763ee593809b56bafb0e5529f08c515d49a5
Ayush Agrwal <ayushagarwal22@gmail.com>
2018-02-18 18:22:45 +0530

    Methos added

--- src/com/ayush/MyStack.java
+++ src/com/ayush/MyStack.java
@@ -1,5 +1,33 @@
 package com.ayush;
 
-public class MyStack {
+public class MyStack<T> {
+
+	private static class StackNode<T> {
+		private T data;
+		private StackNode<T> next;
+
+		public StackNode(T data) {
+			this.data = data;
+		}
+
+		private StackNode<T> top;
+
+		public void push(T data) {
+			StackNode<T> node = new StackNode(data);
+			node.next = top;
+			top = node;
+		}
+
+		public T pop() {
+			if (top == null) {
+				System.out.println("Stack is empty");
+			}
+			T item = top.data;
+			top = top.next;
+			return item;
+
+		}
+
+	}
 
 }
